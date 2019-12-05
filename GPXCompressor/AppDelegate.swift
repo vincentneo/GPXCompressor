92144b4b96362acb0cdaebd8414707d2d09e4de5
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2019-12-05 17:01:05 +0800

    Initial Commit

---
+++ GPXCompressor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GPXCompressor
+//
+//  Created by Vincent on 5/12/19.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ GPXCompressor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GPXCompressor
+//
+//  Created by Vincent on 5/12/19.
+//  Copyright © 2019 Vincent. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
